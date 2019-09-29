bed0269b61539568812372e850f5a6257afb581a
Huy <danghuy1994@gmail.com>
2019-09-30 01:20:48 +0700

    Refactor codes

--- src/utils/getHardwareId.js
+++ src/utils/getHardwareId.js
@@ -1,7 +1,8 @@
 import { isEmpty } from 'lodash';
 
-const electron = window.require('electron');
-const nodeMachineId = electron.remote.require('node-machine-id');
+import { electronImport } from './electron';
+
+const nodeMachineId = electronImport('node-machine-id');
 
 export default () => {
   let hardwareId = localStorage.getItem('hardwareId');
